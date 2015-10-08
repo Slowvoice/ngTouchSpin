56b9459533bec1d4e9f08df6cf1e4722964efe42
Joseph Dunn <joseph@tx-0.org>
2015-10-08 16:33:36 -0500

    No keydown event

--- src/js/ngTouchSpin.js
+++ src/js/ngTouchSpin.js
@@ -122,7 +122,7 @@ angular.module('jkuri.touchspin', [])
                 }
             });
 
-            $body.bind('keydown', function(event) {
+            /*$body.bind('keydown', function(event) {
                 if (!scope.focused) {
                     return;
                 }
@@ -138,7 +138,7 @@ angular.module('jkuri.touchspin', [])
                 }
 
                 scope.$apply();
-            });
+            });*/
 
         },
         template: 
